7c5a2a6c7da6a51df6940b269cc94da3906c0f55
RobertoC95 <roberto.couto.1095@gmail.com>
2025-04-18 16:14:37 +0100

    ex01

---
+++ FichaJava/src/FichaPratica1/EX01.java
@@ -0,0 +1,32 @@
+package FichaPratica1;
+
+import com.sun.tools.javac.Main;
+
+import java.util.Scanner;
+
+public class EX01 {
+
+    public static void main(String[] args) {
+
+        //scanner
+        Scanner input = new Scanner(System.in);
+
+        //Declarar Variaveis
+        int numero1, numero2, soma;
+
+        //ler1
+        System.out.println("Introduza o 1º numero");
+        numero1= input.nextInt();
+
+        //ler2
+        System.out.println("Introduza o 2º numero");
+        numero2= input.nextInt();
+
+        //Soma
+        soma= numero1 + numero2;
+        System.out.println("soma " + soma) ;
+    }
+}
+
+
+
