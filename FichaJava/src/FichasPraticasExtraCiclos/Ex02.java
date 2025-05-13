6fdb0ddad8e3c3ea1ea7d2a8879c0082e85ec81f
RobertoC95 <roberto.couto.1095@gmail.com>
2025-05-13 06:56:26 +0100

    0

--- FichaJava/src/FichasPraticasExtraCiclos/Ex02.java
+++ FichaJava/src/FichasPraticasExtraCiclos/Ex02.java
@@ -13,4 +13,4 @@ public class Ex02 {
         }
     }
 }
-}
+
---
+++ FichaJava/src/FichasPraticasExtraCiclos/Ex04_2.java
@@ -0,0 +1,16 @@
+package FichasPraticasExtraCiclos;
+
+public class Ex04_2 {
+    public static void main(String[] args) {
+
+        int linhas = 5;
+
+        for (int i = 1; i <= linhas; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
