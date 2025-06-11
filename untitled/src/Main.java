c18a2ef535757d1493c829aec55bb4b398af2578
RobertoC95 <roberto.couto.1095@gmail.com>
2025-06-11 16:43:53 +0100

    11/06

---
+++ untitled/src/Main.java
@@ -0,0 +1,56 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        //mudar de private para protected nas super classes(abstractas)
+
+        /*
+
+        enchatments:
+
+        -cavaleiro
+            arma:
+                .sharpness V __ damage*= 3
+                .fire aspect __ damage*=1.5
+            armadura:
+                .protection IV __ armor = 80
+
+        -feiticeiro
+            .Harry's wand damage*=1.5
+            .Elder wand damage*=2.5
+            .
+
+        -arqueiro
+            .power V __ damage*=2.5
+            .punch II __ damage+=10
+            .flame __ damage*1.5
+
+
+        items para venda:
+            potion of healing
+            potion of regeneration
+            potion of strength
+            potion of weakness
+            golden apple
+            notch apple
+            totem of undying
+            sharpness V book
+            fire aspect book
+            protection IV book
+            power V book
+            punch II book
+            flame book
+            sharpness V book
+            Harry's wand
+            Elder wand
+            Holy and grenade of antioch
+            water bucket
+
+
+hp-=(getCurrentHP - (npc.getDano * ((100-armor)/100))
+         */
+
+    }
+}
---
+++ untitled/src/story.java
@@ -0,0 +1,149 @@
+public class story {
+
+    /*
+    intro
+
+    escolher class
+
+    1-cavaleiro
+    2-arqueiro
+    3-feiticeiro
+
+    switch(opcao)
+    1- Arthur King of the Britons
+    2- Steve
+    3- Ron Weasley
+
+
+    escolher seed
+
+    1- random seed
+    2- costum seed
+
+    switch(opcao)
+    1- "it would not make a difference anyway"
+    2- "Choose the number you want there's only one seed and thats the one you're playing"
+
+
+    "You spawn in the middle of nowhere and a weird guy with 2 lamas on a lead shows up"
+    *villager noise*
+
+    Wandering trader
+    10 items random
+
+    do{
+    pick item
+    if (goldH > preco)
+    inventario.add(escolha)
+    goldH -= preco
+    else
+    "Aqui não se faz fiado"
+    } while (!opcao.equals(exit)
+
+    primeira opção de caminho
+
+    1-Seguir para a direita
+    2-Seguir para a esquerda
+    3-
+
+    switch(opcao)
+
+    case1: continua o caminho
+    case2: continua o caminho
+    case3: "it's to late to go back you're going to play the game, and you're going to like it"
+            //novo menu com apenas as duas primeiras opções
+
+
+    //LEFT
+
+    "You find yourself in the middle of a dark oak forest..."
+    "The large and tall trees provide a lot of shadow for mobs to hide and survive..."
+    "But you're thirsty for blood..."
+    "So lets find you a fight"
+    "You finally spot a zombie"
+    "Let's put your fighting skills to the test"
+
+    start of fight sequence
+
+   show npc details
+
+    if(heroclass = cavaleiro)
+       do
+
+        npc attacks
+        show hero details
+        player chooses attack
+        player attacks
+        show npc details
+        if(ncpHP<1)
+            "enemy defeated"
+        if(heroHP<1)
+            "you dead"
+
+        while(npcHP<1 || heroHP<1)
+
+    else
+        do
+        player chooses attack or consumable to be used
+        do{
+        switch (opcao)
+            case 1: normal attack
+            case 2: special attack
+        while(opcao<1 || opcao>2)
+        player attacks
+        show npc details
+        npc attacks
+        show hero details
+        if(ncpHP<1)
+            "enemy defeated"
+        if(heroHP<1)
+            "you dead"
+        while(ncpHP<1 || heroHP<1)
+
+        if(heroHP<1) restart game
+
+        //end of fight sequence
+
+        "Very well done" + this.name + "you've beaten your first opponent"
+
+        "Let's keep moving..."
+
+        "You spot a cave in the distance..."
+        "There is light inside"
+        "That probably means someone has been here already"
+
+
+        hmmm
+        the wandering trader is here again, it might be a sign to restock or buy something for the first time, remember the trader has a family to feed
+
+        *while entering the cave you ear the sound of a nether portal being used, it seems like it coming from bellow*
+
+        *you get curious, you cant help yourself, and you go looking for it*
+
+        you have 2 options
+            1- Mine straight down until you find it
+            2- go down into the cave and follow the trace left by that mysterious person
+
+
+        switch(opcao)
+            1- fall into lava and die if water bucket survive
+            2- kamikaze creeper and die 100damage
+
+
+
+        //RIGHT
+
+
+//this might be the final fight
+        "As you walk the fields alone, you see something massive and shiny, you wonder what it might be so you decide to take a closer look..."
+        "you get closer and realize its Pedro Couto's massive head "
+        "he challenged you for a fight but be careful, his only attack his the headbutt"
+        "you can only imagine what something that big will do to you"
+
+        Pedro's Strenght 200
+
+
+
+
+     */
+}
